09c5b1a56c7f25328dbd76c33c01674a84ed311a
advaithm582 <83835839+advaithm582@users.noreply.github.com>
2023-07-05 20:28:56 +0530

    Final

--- gt_can/tg_message/messager_channel.py
+++ gt_can/tg_message/messager_channel.py
@@ -1,4 +1,5 @@
 ﻿import json
+import logging
 import time
 
 from gt_can.tg_message.helpers import send_message
@@ -7,7 +8,7 @@ import gt_can.crawlers.beautifulsoup as bsc
 MSG_COURSE = "<b>SECTION DETAILS FOR {subj} {code}, TERM {term}</b>\n\n"
 MSG_MORETHAN5 = ("➡️ Section <b>{subj} {code} - {sec}</b> "
                  "(CRN#: {crn}) is AVAILABLE ({avail}/{total} remaining)\n")
-MSG_LESSOR5 = ("➡️ [<b><i>Quick! Less than 5 left!</i></b>] "
+MSG_LESSOR5 = ("➡️ [<b><i>Quick! Less than 6 left!</i></b>] "
                "Section <b>{subj} {code} - {sec}</b> "
                "(CRN#: {crn}) is FILLING UP ({avail}/{total} remaining)\n")
 MSG_ONLYWAIT = ("➡️ Section <b>{subj} {code} - {sec}</b> "
@@ -15,14 +16,38 @@ MSG_ONLYWAIT = ("➡️ Section <b>{subj} {code} - {sec}</b> "
                 "({avail}/{total} remaining)\n")
 
 
-def run_plan_item(api_key, channel, courses):
+logger = logging.getLogger(__name__)
+
+
+def run_plan_item(api_key, channel, courses, prefs):
     for course in courses:
         cmsg = MSG_COURSE.format(subj=course[1],
                                  code=course[2],
                                  term=course[0])
+        try:
+            sections = tuple(
+                bsc.discover_sections_crn(course[0], course[1], course[2])
+            )
+        except:
+            logger.error("Failed to discover sections of {}".format(course), 
+                         exc_info=True)
+            if prefs["error_messages"]:
+                send_message(api_key, channel,
+                             "ERROR: could not discover sections of "
+                             f"{course}")
+            continue
 
-        for crn, sec in bsc.discover_sections_crn(course[0], course[1], course[2]):
-            avail = bsc.get_class_seats_tc(course[0], crn)
+        for crn, sec in sections:
+            try:
+                avail = bsc.get_class_seats_tc(course[0], crn)
+            except:
+                logger.error("Failed to seats in TERM %s CRN %d", course[0],
+                             crn, exc_info=True)
+                if prefs["error_messages"]:
+                     cmsg+= ("⚠️ ERROR: could not check seats in "
+                             f"CRN# {crn} (term {course[0]}) \n")
+                continue
+            
 
             if avail[0][2] > 5:
                 cmsg += MSG_MORETHAN5.format(subj=course[1],
@@ -49,7 +74,7 @@ def run_plan_item(api_key, channel, courses):
                                             avail=avail[1][2],
                                             total=avail[1][0])
             
-            time.sleep(1)
+            time.sleep(prefs["pause_interval"]["section"])
 
         cmsg += ("\n⚠️⚠️⚠️<b>WARNING</b> These may include courses from "
                  "other GT campuses (e.g. GT Lorraine), so check before "
@@ -57,7 +82,9 @@ def run_plan_item(api_key, channel, courses):
 
         a = send_message(api_key, channel, cmsg)
         if not a.status_code == 200:
-            print("ERROR:", a.text)
+            logger.error("Telegram Send Error: %s", a.text)
+
+        time.sleep(prefs["pause_interval"]["course"])
 
 
 def bootstrap(file="telegram_config.json"):
@@ -69,5 +96,5 @@ def bootstrap(file="telegram_config.json"):
 
 def run_all_plans(data):
     for channel, courses in data["message_plan"].items():
-        run_plan_item(data["bot_token"], channel, courses)
-        time.sleep(5)
+        run_plan_item(data["bot_token"], channel, courses, data["preferences"])
+        time.sleep(data["preferences"]["pause_interval"]["plan"])
